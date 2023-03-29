36dfe167ca73c844824e6ae3dd57705f753299fd
soso2001-afk <mahmoudisofiane2019@gmail.com>
2023-03-29 15:54:22 +0200

    1.6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar';
 import Hiro from './components/Hiro';
 import Skills from './components/Skills'
@@ -11,10 +11,10 @@ import 'aos/dist/aos.css';
 
 
 function App() {
-    useEffect(() => {
-      document.title = 'Damasukma Trihanandi';
-      AOS.init();
-    }, []);
+  useEffect(() => {
+    document.title = 'MAHMOUDI sofiane';
+    AOS.init();
+  }, []);
   return (
     <div className="px-6 lg:px-20 xl:px-36 bg-dark-500">
       <Navbar />
